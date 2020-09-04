6c910bad0f5579fc9d64edae1bbac408464ffb03
3EGaming <3EGaming@users.noreply.github.com>
2020-09-04 20:10:34 +0100

    Add amazoncoco

---
+++ cocos/secretcocos/amazoncoco/rs1.js
@@ -0,0 +1,5 @@
+function gotofreewifi() { 
+alert("Thankyou for ordering 3,000 coco rabit foods!");
+window.location.href = "https://cocowebsite.github.io/";
+window.location.replace("https://cocowebsite.github.io/")
+}
--- files/eastercoco.js
+++ files/eastercoco.js
@@ -59,4 +59,6 @@ document.getElementById("ZAK").innerHTML = "<P>Its Halloween. Coco is scared tha
 //check for url bar secrets
 if (urlVariables["coco"] == "poor") {
 document.getElementById("ZAK").innerHTML = "<P>coco is poor. coco is sad. have a coco coin</P><img src='./../files/cococoin.png'>";
+} else if (urlVariables["amazon"] == "" or urlVariables["coco"] == "amazon") { 
+window.location.replace("./../cocos/secretcocos/amazoncoco/")
 }
