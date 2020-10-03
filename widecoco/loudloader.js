85125973c7312388cfdb600090dc9802432b2723
3EGaming <3EGaming@users.noreply.github.com>
2020-10-03 17:06:09 +0100

    Add louder to wide coco

---
+++ widecoco/loudloader.js
@@ -0,0 +1,6 @@
+document.getElementById("extraloud").innerHTML = "";
+
+if (urlVariables["loud"] == "")
+{
+    document.getElementById("extraloud").innerHTML = "<audio controls><source src=\"Wide_Putin_music_LOUDER.mp3\" type=\"audio/mpeg\"></audio><audio controls><source src=\"Wide_Putin_music_LOUDERER.mp3\" type=\"audio/mpeg\"></audio><audio controls><source src=\"Wide_Putin_music_LOUDERERER.mp3\" type=\"audio/mpeg\"></audio><audio controls><source src=\"Wide_Putin_music_LOUDERERERER.mp3\" type=\"audio/mpeg\"></audio>";
+}
