07816f8488354fdf57d184081431c87ba6ca2005
3EGaming <3EGaming@users.noreply.github.com>
2020-12-10 17:14:20 +0000

    Finish the coco website. R.I.P

--- cocos/luckywheel/clientgrab/chancecheck.js
+++ cocos/luckywheel/clientgrab/chancecheck.js
@@ -1,7 +1,13 @@
+if (cocodead = false)
+{
 luckyrandom = Math.floor(Math.random() * 11)
 if (luckyrandom == 5)
 {
     document.getElementById("allContent").innerHTML = '<div id="outPopUp"><H1>Congradulations!</H1><H3>You win luckyspin on wheel!</H3><img src="./cocos/luckywheel/prizeclaim/continuenoscam.png" height="140" width="250" onClick="window.location.replace(\'./cocos/luckywheel/\');"></div>';
 } else {
     window.location.replace("./home/");
+}
+} else {
+document.getElementById("allContent").innerHTML = "";
+window.location.replace("./rip/")
 }
---
+++ files/cocolifestatus.js
@@ -0,0 +1,1 @@
+cocodead = false;
